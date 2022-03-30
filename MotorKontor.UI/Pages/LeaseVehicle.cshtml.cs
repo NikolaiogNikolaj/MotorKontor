2cdac64ffc97341478e4cad4ce25be8c346a6708
Nikolaj Garro Throksø <Tec@SKAB2-PC-02>
2022-03-30 09:32:14 +0200

    test

---
+++ MotorKontor.UI/Pages/LeaseVehicle.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MotorKontor.UI.Pages
+{
+    public class LeaseVehicleModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
